b0dfa53e95b951d706fa05f1028798328e539f21
Jurandi Carvalho <jurandi.carvalho@outlook.com>
2024-01-17 07:26:37 -0300

    supero plus Español

---
+++ language.js
@@ -0,0 +1,18 @@
+var translations = {
+    "portuguese": {
+        "ENGSOFT_LINK": "Link para Engenharia de Software",
+        "MOD013_LINK": "Link para o Módulo 013",
+        "MOD010_LINK": "Link para o Módulo 010",
+        "MOD090_LINK": "Link para o Módulo 090",
+        "MOD140_LINK": "Link para o Módulo 140"
+    },
+    "english": {
+        "ENGSOFT_LINK": "Link to Software Engineering",
+        "MOD013_LINK": "Link to Module 013",
+        "MOD010_LINK": "Link to Module 010",
+        "MOD090_LINK": "Link to Module 090",
+        "MOD140_LINK": "Link to Module 140"
+    }
+};
+
+var lang = translations[selectedLanguage];
